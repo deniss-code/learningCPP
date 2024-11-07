4a5d02b72ee49abd83cdc1df32c5f5e01d17d2c8
deniscodes <186865079+deniss-code@users.noreply.github.com>
2024-11-07 16:22:33 +0100

    No more stack overflow, but the result is not correct

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <math.h>
+# define M_PI	3.14159265358979323846  /* pi */
 
+
+class Errors {
+public:
+	void nullNumber() {
+		std::cout << "Doing this calculation through 0 does not work!\n";
+	}
+};
 
 class Calculator {
 public:
@@ -15,35 +24,71 @@ public:
 	double subtraction(double a, double b) {
 		return a / b;
 	}
+	double modulo(double a, double b) {
+		return std::fmod(a, b);
+	}
+	double sin(long double angle) {
+		return std::sin(angle);  // Use std::sin to avoid recursion
+	}
+	double cos(long double angle) {
+		return std::cos(angle * M_PI / 180);  // Use std::cos
+	}
+	double tan(double angle) {
+		return std::tan(angle * M_PI / 180);  // Use std::tan
+	}
 };
 
 
 int main() {
 	Calculator calc;
-	char choice;
-	double num1, num2, result = 0;
+	std::string choice;
+	double num1 = 0, num2 = 0, angleNum, result = 0;
 	std::cout << "Choose your calculation method\n";
-	std::cout << "+ | - | * | / \n";
+	std::cout << "1. Arithmetic: + | - | * | / \n";
+	std::cout << "2. Trigonometric: sin | cos | tan \n";
 	std::cin >> choice;
 	
-	std::cout << "Enter your numbers\n";
-	std::cin >> num1 >> num2;
-	switch (choice) {
-	case '+':
+	if (choice == "sin" || choice == "cos" || choice == "tan") {
+		std::cout << "Enter your number:\n";
+		std::cin >> angleNum;
+	}
+	else if (choice == "+" || choice == "-" || choice == "*" || choice == "/") {
+		std::cout << "Enter your numbers\nFirst Number: ";
+		std::cin >> num1;
+		std::cout << "Second Number: ";
+		std::cin >> num2;
+	}
+	else {
+		std::cout << "Wrong input!";
+		return 0;
+	}
+
+
+	if (choice == "+") {
 		result = calc.add(num1, num2);
-		break;
-	case '-':
+	}
+	else if (choice == "-") {
 		result = calc.minus(num1, num2);
-		break;
-	case '*':
+	}
+	else if (choice == "*") {
 		result = calc.multiplication(num1, num2);
-		break;
-	case '/':
+	}
+	else if (choice == "/") {
 		result = calc.subtraction(num1, num2);
-		break;
-	default:
+	}
+	else if (choice == "sin") {
+		result = calc.sin(num1);
+	}
+	else if (choice == "cos") {
+		result = calc.cos(num1);
+	}
+	else if (choice == "tan") {
+		result = calc.tan(num1);
+	}
+	else{
 		std::cout << "Wrong input!\n";
 	}
+
 
 	std::cout << "The result is " << result;
 
